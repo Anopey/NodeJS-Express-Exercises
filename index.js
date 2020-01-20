4cb96a29abbaed83571f3a10f6c4731b22f50cae
Ozan Ürkmez <arkantos250@gmail.com>
2020-01-20 19:49:46 +0300

    Add basic page file indexing and class

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function(req,res){
-    res.render("home.ejs");
-});
+/* #region basic pages */
 
-app.listen(3000, function(){
+class basicPage {
+
+    /**
+     * 
+     * @param {string} description 
+     * @param {string} extension 
+     * @param {string} fileName
+     */
+    constructor(description, extension, fileName, argumentsDictionary) {
+        this.description = description;
+        this.extension = extension;
+        this.fileName = fileName;
+        this.argumentsDictionary = argumentsDictionary;
+    }
+
+}
+
+
+var basicPages = [new basicPage("The home page.", "/", "home.ejs")];
+
+basicPages[0].argumentsDictionary =  {basicPages : basicPages};
+
+basicPages.forEach(function (page) {
+    app.get(page.extension, function (req, res) {
+        res.render(page.fileName, page.argumentsDictionary);
+    });
+})
+
+
+/* #endregion */
+
+
+
+
+
+app.listen(3000, function () {
     console.log("Server is listening on port 3000...");
 })
