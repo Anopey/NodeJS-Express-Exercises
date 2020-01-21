cc4c34088dd63819bcd5adc57e392ed166604532
Ozan Ürkmez <arkantos250@gmail.com>
2020-01-21 11:29:06 +0300

    Add hi pages and dynamic variables for basic pages.

--- index.js
+++ index.js
@@ -21,13 +21,26 @@ class basicPage {
 }
 
 
-var basicPages = [new basicPage("The home page.", "/", "home.ejs")];
+var basicPages = [new basicPage("The home page.", "/", "home.ejs"),
+new basicPage("static Hi page.", "/hi", "hi.ejs"),
+new basicPage("hi page, but it can say your name!", "/hi/:name", "hiDynamic.ejs", {name : ""})]; //if left as empty string, the name of the key will be used within params
 
 basicPages[0].argumentsDictionary =  {basicPages : basicPages};
 
 basicPages.forEach(function (page) {
     app.get(page.extension, function (req, res) {
+        var dynamicParams = [];
+        for(var key in page.argumentsDictionary){
+            if(page.argumentsDictionary[key] === ""){
+                page.argumentsDictionary[key] = req.params[key];
+                dynamicParams.push(key);
+            }
+        }
         res.render(page.fileName, page.argumentsDictionary);
+        console.log("A get request to " + page.extension + " has been made!")
+        for(var key in dynamicParams){
+            page.argumentsDictionary[dynamicParams[key]] = ""; 
+        }
     });
 })
 
