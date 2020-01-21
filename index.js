789bee6abc250b98e50b9877e479be282d8d7251
Ozan Ürkmez <arkantos250@gmail.com>
2020-01-21 15:59:12 +0300

    added friends lister

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
-app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 /* #region basic pages */
 
@@ -25,23 +27,28 @@ class basicPage {
 
 var basicPages = [new basicPage("The home page.", "/", "home.ejs"),
 new basicPage("static Hi page.", "/hi", "hi.ejs"),
-new basicPage("hi page, but it can say your name!", "/hi/:name", "hiDynamic.ejs", {name : ""})]; //if left as empty string, the name of the key will be used within params
+new basicPage("hi page, but it can say your name!", "/hi/:name", "hiDynamic.ejs", { name: "" }), //if left as empty string, the name of the key will be used within params
+new basicPage("a list made just for your friends :)", "/friends", "friends.ejs")]; 
 
-basicPages[0].argumentsDictionary =  {basicPages : basicPages};
+basicPages[0].argumentsDictionary = { basicPages: basicPages };
+
+//info provided by some basic pages
+var friends = [];
+basicPages[3].argumentsDictionary = {friends: friends};
 
 basicPages.forEach(function (page) {
     app.get(page.extension, function (req, res) {
         var dynamicParams = [];
-        for(var key in page.argumentsDictionary){
-            if(page.argumentsDictionary[key] === ""){
+        for (var key in page.argumentsDictionary) {
+            if (page.argumentsDictionary[key] === "") {
                 page.argumentsDictionary[key] = req.params[key];
                 dynamicParams.push(key);
             }
         }
         res.render(page.fileName, page.argumentsDictionary);
         console.log("A get request to " + page.extension + " has been made!")
-        for(var key in dynamicParams){
-            page.argumentsDictionary[dynamicParams[key]] = ""; 
+        for (var key in dynamicParams) {
+            page.argumentsDictionary[dynamicParams[key]] = "";
         }
     });
 })
@@ -49,8 +56,18 @@ basicPages.forEach(function (page) {
 
 /* #endregion */
 
+/* #region basic posts */
 
+app.post("/friend/addfriend", function(req, res) {
+    var friendName = req.body.friendName;
+    if(friendName != null){
+    console.log("A post request to friend/addfriend has been made with data " + friendName);
+    friends.push(friendName);
+    }
+    res.redirect("/friends");
+});
 
+/* #endregion */
 
 
 app.listen(3000, function () {
